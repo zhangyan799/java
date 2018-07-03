2491e2f1e825350bd24ab0b761e66ec2813eb26f
qingyun <qingyun@zhangyan.local>
2018-07-03 16:09:14 +0800

    2

--- Student.java
+++
@@ -1,8 +0,0 @@
-public class Student {
-	public static void main(String[] args) {
-		System.out.println("学生类");
-		System.out.println("学号");
-		System.out.println("成绩");
-		System.out.println("30分");
-	}
-}
